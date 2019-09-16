5b6ef0cbf338ed95b632ed7c069148887de6c4f0
西山桂士朗 <nishiyamakatsurashirou@mba.local>
2019-09-16 16:54:52 +0900

    add users model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+[
+  ['test3@test.com','333333','kei'],
+  ['test4@test.com','444444','k']
+].each do |mail, pass, name|
+  User.create!(
+    {email: mail, password: pass, name: name, confirmed_at: Time.now}
+  )
+end
